62d3d998362e87125e3b030821839a3fdb8b5b58
Evan Prodromou <evan@fuzzy.io>
2015-04-13 11:24:18 -0400

    Support SSL termination and pass-through

--- haproxy-start.sh
+++ haproxy-start.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+# Written in 2015 by Evan Prodromou <evan@prodromou.name>
+#
+# To the extent possible under law, the author(s) have dedicated all copyright and
+# related and neighboring rights to this software to the public domain worldwide.
+# This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along with
+# this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
 set -e
-set -x
 
 export TEMPLATE=/usr/local/share/haproxy.cfg.tmpl
 export CONFFILE=/usr/local/etc/haproxy/haproxy.cfg
 export SCORCH=/usr/bin/scorch
 export HAPROXY=/usr/local/sbin/haproxy
 
-echo START
+if [[ "x$KEY" != "x" ]]; then
+  echo $KEY $CERT > /usr/local/etc/haproxy/haproxy.pem
+fi
 
 $SCORCH -e $TEMPLATE > $CONFFILE
 
-echo NEXT
-
 exec $HAPROXY -db -f $CONFFILE
-
-echo DONE
